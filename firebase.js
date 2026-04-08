b14bd60ac8024043dbad21ed1fcd616c612a852f
uchiko3311 <135709125+uchiko3311@users.noreply.github.com>
2026-04-08 22:30:12 +0400

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCuFjbwcI3up46IoNEJfayWyGnXvABsOfM",
+  authDomain: "swap-5d311.firebaseapp.com",
+  databaseURL: "https://swap-5d311-default-rtdb.firebaseio.com",
+  projectId: "swap-5d311",
+  storageBucket: "swap-5d311.appspot.com",
+  messagingSenderId: "444445838698",
+  appId: "1:444445838698:web:18ab8580aa018d0236a913",
+  measurementId: "G-HS88H5PQ9P"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
