1a444fa8ec74ba52b9a52dea5ea80210c40eb699
uchiko3311 <135709125+uchiko3311@users.noreply.github.com>
2026-04-08 22:41:48 +0400

    Delete payment.js

--- payment.js
+++
@@ -1,9 +0,0 @@
-async function vipPayment(){
-  const res = await fetch('https://your-server.com/api/payment/create-payment',{
-    method:'POST',
-    headers:{'Content-Type':'application/json'},
-    body:JSON.stringify({userId:'123',amount:4.99})
-  });
-  const data = await res.json();
-  window.open(data.paymentUrl,'_blank');
-}
