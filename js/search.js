eb3fbdd114909af7bb1f3ccc6ff4aba00dff99e3
uchiko3311 <135709125+uchiko3311@users.noreply.github.com>
2026-04-11 19:36:09 +0400

    Delete js/search.js

--- js/search.js
+++
@@ -1,6 +0,0 @@
-export function filterItems(items, category, minPrice) {
-    return items.filter(item => 
-        (category === "all" || item.category === category) &&
-        (item.price >= minPrice)
-    );
-}
