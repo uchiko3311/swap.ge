8dbeb02a4d739408fa175da7778c5f4172a712cc
uchiko3311 <135709125+uchiko3311@users.noreply.github.com>
2026-04-12 18:57:04 +0400

    Create errorMiddleware.js

---
+++ middleware/errorMiddleware.js
@@ -0,0 +1,11 @@
+function errorMiddleware(err, req, res, next){
+
+    console.error(err.stack);
+
+    res.status(500).json({
+        msg:"Server Error",
+        error: err.message
+    });
+}
+
+module.exports = errorMiddleware;
